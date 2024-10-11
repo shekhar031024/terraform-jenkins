346481458d5b6cbf94eeca1b9d0fbf483ff6489f
shekhar031024 <shekhar031024@gmail.com>
2024-10-11 16:42:13 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,6 @@
 provider "google" {
-      credentials = file("avid-booster-437510-t4-64d9ffda9651.json")
-
-  project = "avid-booster-437510-t4"
+  credentials = file("avid-booster-437510-t4-1a349481784e.json")
+  project = "avid-booster-437510-t4" 
   region  = "us-central1"
 }
 
